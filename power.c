7d5bbd553d4b701c6cbcc6d5af5ee9c3632aadbc
Michał Suszko <michal@dry.pl>
2016-02-07 14:10:04 +0100

    Use bit masks

--- power.c
+++ power.c
@@ -5,55 +5,73 @@
 
 int8_t first_stop = -1;
 int8_t next_stop = -1;
+int8_t off_midstop = -1;
 // stop_idx -> power
 spoint stops[PIN_NUM];
 // pin_idx -> stop
 int8_t pin2stop[PIN_NUM];
 // stop_idx -> pin[]
-int8_t stop2pins[PIN_NUM][PIN_NUM];
+uint8_t stop2pins[PIN_NUM][3];
+uint8_t pin_mask[3];
+
+struct
+{
+	int8_t port_no;
+	uint8_t mask;
+} ports[] = {
+	{2, PD5}, //  1   0
+	{2, PD4}, //  2   1
+	{2, PD6}, //  3   2
+	{2, PD7}, //  4   3
+	{0, PB1}, //  5   4
+	{0, PB0}, //  6   5 
+	{0, PB2}, //  8   6
+	{1, PC1}, //  9   7
+	{1, PC0}, // 10   8
+	{1, PC2}, // 11   9
+	{1, PC3}, // 12  10
+	{1, PC4}, // 13  11
+	{1, PC5}  // 14  12
+};
+
+
+void set_pin(uint8_t *reg, int8_t pin)
+{
+	*(reg + ports[pin].port_no) &= ~(_BV(ports[pin].mask));
+};
+
+void unset_pin(uint8_t *reg, int8_t pin)
+{
+	*(reg + ports[pin].port_no) |= _BV(ports[pin].mask);
+};
 
 void power_init(void)
 {
-	int8_t idx, sidx;
+	int8_t idx;
 
 	for (idx=0; idx<PIN_NUM; idx++) {
 		stops[idx].power = 0;
 		stops[idx].next = -1;
 		pin2stop[idx] = -1;
-		for (sidx=0; sidx<PIN_NUM; sidx++) {
-			stop2pins[idx][sidx] = -1;
-		}
+		stop2pins[idx][0] = 0;
+		stop2pins[idx][1] = 0;
+		stop2pins[idx][2] = 0;
+		set_pin(pin_mask, idx);
 	}
 }
 
 
 void set_power(int8_t pin, uint8_t power)
 {
-	int8_t idx, prev_idx, stop = 0;
-	int8_t zero_idx = 0;
+	int8_t zero_idx, idx, stop = 0;
 	uint8_t lesser_power = 0;
 
 	if (pin2stop[pin] != -1) {
 		// remove pin from stop data
 		stop = pin2stop[pin];
-		// stop_pins = stop2pins[stop];
-		zero_idx = 0;
-		for (idx=0; idx<PIN_NUM; idx++) {
-			if (stop2pins[stop][idx] == pin) {
-				stop2pins[stop][idx] = -1;
-				zero_idx = idx;
-			} else if (stop2pins[stop][idx] == -1) {
-				prev_idx = idx - 1;
-				if (prev_idx > zero_idx) {
-					// move last value to empty slot
-					stop2pins[stop][zero_idx] = stop2pins[stop][prev_idx];
-					stop2pins[stop][prev_idx] = -1;
-				}
-				break;
-			}
-		}
+		unset_pin(stop2pins[stop], pin);
 		// remove stop if empty
-		if (zero_idx == 0) {
+		if (stop2pins[stop][0] == 0 && stop2pins[stop][1] == 0 && stop2pins[stop][2] == 0) {
 			if (first_stop == stop) {
 				first_stop = stops[stop].next;
 			} else {
@@ -90,18 +108,12 @@ void set_power(int8_t pin, uint8_t power)
 	}
 	pin2stop[pin] = stop;
 
-	// add pin to stop data at first empty slot
-	for (idx=0; idx<PIN_NUM; idx++) {
-		if (stop2pins[stop][idx] == 0) {
-			stop2pins[stop][idx] = pin;
-			break;
-		}
-	}
+	set_pin(stop2pins[stop], pin);
 }
 
 void zero_cross(void)
 {
-	TCNT1 = 0;   //reset timer - count from zero
+	TCNT1 = 0;   // reset timer - count from zero
 	if (first_stop == -1) {
 		TIMSK = 0;
 		return;
@@ -113,22 +125,38 @@ void zero_cross(void)
 
 void timer_alarm(void)
 {
-	int8_t idx, stop, pin;
+	int8_t stop;
+
+	// execute off midstop
+	if (off_midstop != -1) {
+		PORTB = stop2pins[off_midstop][0];
+		PORTC = stop2pins[off_midstop][1];
+		PORTD = stop2pins[off_midstop][2];
+		off_midstop = -1;
+		OCR1A = delays[stops[next_stop].power];
+		return;
+	}
 
 	if (stops[next_stop].next == -1) {
-		next_stop = first_stop;
+		// this is last stop; zero all pins and disable timer
 		TIMSK = 0;
+		PORTB &= pin_mask[0];
+		PORTC &= pin_mask[1];
+		PORTD &= pin_mask[2];
+		next_stop = first_stop;
+		return;
+	}
+	// set the next stop
+	stop = next_stop;
+	next_stop = stops[stop].next;
+	// set timer comparator to fire interrupt on next stop
+	if ((stops[stop].power + 2) > stops[next_stop].power) {
+		off_midstop = stop;
+		OCR1A = delays[stops[stop].power+1];
 	} else {
-		stop = next_stop;
-		next_stop = stops[stop].next;
-		OCR1A = delays[stops[stop].power];
+		OCR1A = delays[stops[next_stop].power];
 	}
-	for (idx=0; idx<PIN_NUM; idx++) {
-		pin = stop2pins[stop][idx];
-		if (pin == -1) {
-			break;
-		} else {
-			// enable
-		}
-	}
+	PORTB = stop2pins[stop][0];
+	PORTC = stop2pins[stop][1];
+	PORTD = stop2pins[stop][2];
 }
